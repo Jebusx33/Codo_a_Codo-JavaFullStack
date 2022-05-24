3012e51fbe262b561247623f4fc54cc41da12aae
jebusx33 <gabrielj82@gmail.com>
2022-05-24 10:19:40 -0300

    trabajo integrador con bootstrap y js

--- 04-Trabajo_práctico_integrador/js/app.js
+++ 04-Trabajo_práctico_integrador/js/app.js
@@ -3,35 +3,30 @@ function mostrarValor() {
     var cod = document.getElementById("descuento").value;
     const pagar = document.getElementById("infoPagar");
     const btnResumen = document.querySelector(".btnResumen");
+ 
+    function ButtonResumen() {
+        btnResumen.addEventListener("click", (e) => {
+            var entradas = parseInt(cantidadTickets * 200);
+            if (cod == "0") {
+                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.80));
+            }
+            if (cod == "1") {
+                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.50));
+            }
+            if (cod == "2") {
+                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.15));
+            }
+        });
+    }
+     
+
     if (isNaN(cantidadTickets)) {
         cantidadTickets = 1;
         const cantidad = document.getElementById("cantidad");
         cantidad.value = "1";
-        btnResumen.addEventListener("click", (e) => {
-            var entradas = parseInt(cantidadTickets * 200);
-            if (cod == "0") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.80));
-            }
-            if (cod == "1") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.50));
-            }
-            if (cod == "2") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.15));
-            }
-        });
+        ButtonResumen();
     } else {
-        btnResumen.addEventListener("click", (e) => {
-            var entradas = parseInt(cantidadTickets * 200);
-            if (cod == "0") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.80));
-            }
-            if (cod == "1") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.50));
-            }
-            if (cod == "2") {
-                pagar.innerHTML = "Total a pagar: $" + (entradas - (entradas * 0.15));
-            }
-        });
+        ButtonResumen();
     }
 }
 
